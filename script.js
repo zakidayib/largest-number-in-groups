003b3df3bc40402913f3fbdfc2d54cca0677bba0
zaki tayyip <zakidayib1997@gmail.com>
2023-06-14 15:24:50 +0300

    largest-number-in-groups-solved

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Good Luck 💪🏾
+ function findLargestNumbers(arr){
+    let lagestArr = [];
+
+    for(let i=0; i<arr.length; i++){
+        
+            lagestArr.push(Math.max(...arr[i]))
+        
+    }
+
+    return (lagestArr)
+}
+
+
+
+
+
+
+
+
+
+
+console.log(findLargestNumbers([[22, 55, 12], [45, 66, 95], [22, 5, 11]]));
